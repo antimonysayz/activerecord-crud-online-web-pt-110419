3b95f553d9f48495567d8ba6027738b4b9d1cf28
Anthony Mai <anthony.mai530@gmail.com>
2020-02-10 01:33:31 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -31,6 +31,7 @@ def can_be_created_in_a_block(args = {})
     m.title = args[:title] || 'Home Alone'
     m.release_date args[:release_date] || 1990
   end
+  
 end
 
 def can_get_the_first_item_in_the_database
